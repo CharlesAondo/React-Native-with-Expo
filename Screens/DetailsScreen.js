c7adcebeb5f77f9f47e908b3a34bdd848223da62
charlestimeless <54895123+charlestimeless@users.noreply.github.com>
2020-09-02 22:47:18 -0300

    Update DetailsScreen.js

--- Screens/DetailsScreen.js
+++ Screens/DetailsScreen.js
@@ -1,32 +1,90 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-
-const DetailsScreen = ({navigation}) => {
-    return (
-      <View style={styles.container}>
-        <Text>Details Screen</Text>
-        <Button
-            title="Go to details again"
-            onPress={() => navigation.push("Details")}
-        />
-        <Button
-            title="Go to home"
-            onPress={() => navigation.navigate("Home")}
-        />
-        <Button
-            title="Go back"
-            onPress={() => navigation.goBack()}
-        />
-      </View>
-    );
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import Header from './Header'
+import Treatments from './DrugDataScreen/Treatment';
+import Therapeutics from './DrugDataScreen/Therapeutics';
+import Pearls from './DrugDataScreen/Pearls';
+import Preacautions from './DrugDataScreen/Precautions'
+const Tab = createMaterialBottomTabNavigator();
+const DetailsScreen = ({ routes, navigation }) => {
+      return (
+            <View style={styles.container}>
+                  <Header />
+                  <DrugTabs />
+            </View>
+      );
 };
+
 
 export default DetailsScreen;
 
+
+const DrugTabs = () => {
+      return (
+            <Tab.Navigator
+                  initialRouteName="Treatments"
+                  activeColor="#5cb85c"
+                  style={{ backgroundColor: 'tomato' }}
+            >
+                  <Tab.Screen
+                        name="Feed"
+                        component={Treatments}
+                        options={{
+                              tabBarLabel: 'Dosages',
+                              tabBarColor: 'green',
+                              tabBarIcon: ({ color }) => (
+                                    <Fontisto name="injection-syringe" size={26} color={color} />),
+                        }}
+                  />
+                  <Tab.Screen
+                        name="Therapeutics"
+                        component={Therapeutics}
+                        options={{
+                              tabBarLabel: 'Therapeutics',
+                              tabBarColor: 'blue',
+                              tabBarIcon: ({ color }) => (
+                                    <Fontisto name="bandage" size={26} color={color} />
+                              ),
+                        }}
+                  />
+                  <Tab.Screen
+                        name="Preacautions"
+                        component={Preacautions}
+                        options={{
+                              tabBarLabel: 'Preacautions',
+                              tabBarColor: '#a94442',
+                              tabBarIcon: ({ color }) => (
+                                    <FontAwesome5 name="exclamation-triangle" size={26} color={color} />
+                              ),
+                        }}
+                  />
+                  <Tab.Screen
+                        name="Pearls"
+                        component={Pearls}
+                        options={{
+                              tabBarLabel: 'Pearls',
+                              tabBarColor: '#808080',
+                              tabBarIcon: ({ color }) => (
+                                    <MaterialCommunityIcons name="information-outline" size={26} color={color} />),
+                        }}
+                  />
+            </Tab.Navigator>
+      );
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
-  },
+      container: {
+            flex: 1,
+            /*   alignItems: 'center',
+              justifyContent: 'center', */
+            paddingTop: 40,
+            backgroundColor: '#fff',
+      }
 });
