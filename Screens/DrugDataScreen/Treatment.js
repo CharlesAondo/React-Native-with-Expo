016ac33d60148ad3ea0f3eb070ffa670faf249a3
charlestimeless <54895123+charlestimeless@users.noreply.github.com>
2020-12-06 14:02:23 -0400

    Update Treatment.js

--- Screens/DrugDataScreen/Treatment.js
+++ Screens/DrugDataScreen/Treatment.js
@@ -4,34 +4,64 @@ import { Text, View, StyleSheet, ScrollView, TouchableOpacity, ActivityIndicator
 import { AuthContext } from '../../components/context'
 
 import * as SQLite from "expo-sqlite"
-import { drugCategoryDrug } from '../../database/drugCategoryDrug';
 
 const db = SQLite.openDatabase('db.db')
 let base64 = require('base-64');
 
+ExecuteQuery = (sql, params = []) => new Promise((resolve, reject) => {
+      db.transaction((trans) => {
+            trans.executeSql(sql, params, (trans, results) => {
+                  resolve(results);
+            },
+                  (error) => {
+                        reject(error);
+                  });
+      });
+});
 
 
 const Treatments = ({ route, navigation, props }) => {
-      const data = React.useContext(AuthContext);
-      const [isLoading, setLoading] = useState(true);
-      const [treatmentData, setDrugs] = useState({})
 
-  
+
+      const drug = React.useContext(AuthContext);
+      const [drugs, setDrugs] = useState({})
+
+
+      const [data, setData] = React.useState({
+            treatment_data: '',
+            isLoading: true
+      })
+
+      const [cateData, setCate] = React.useState({
+            categories: '',
+            loadingCategory: true
+      })
       useEffect(() => {
+            /*
+                       const   getData = async()=>{
+                              let selectQuery = await ExecuteQuery('SELECT * FROM vdi_treatments where drug_id ='+drug.id,[]);
+                              var rows = selectQuery.rows;
             
-            const getData = () => {
+                             setData({
+                                   isLoading:false,
+                                   treatment_data
+                             })
+                              console.log(item)
+                            }
+                            */
 
+            const getData = () => {
                   db.transaction(
                         tx => {
                               tx.executeSql(
-                                    'select * from vdi_treatments where drug_id = ' + data.id, 
+                                    'select * from vdi_treatments where drug_id = ' + drug.id,
                                     [],
                                     (_, { rows: { _array } }) => {
-
-                                          //   console.log("vdi_drugs", _array)
-                                          setDrugs(_array)
-                                          setLoading(false)
-                                            console.log("vdi_drugs", treatmentData)
+                                          setData({
+                                                ...data,
+                                                treatment_data: _array,
+                                                isLoading: false
+                                          })
 
                                     }
                               );
@@ -39,22 +69,73 @@ const Treatments = ({ route, navigation, props }) => {
                   );
 
             }
+            const categories = () => {
+                  db.transaction(
+                        tx => {
+                              tx.executeSql(
+                                    'select * from vdi_drug_category_drug where drug_id = ' + drug.id,
+                                    [],
+                                    (_, { rows: { _array } }) => {
+                                          setCate({
+                                                ...cateData,
+                                                categories: _array,
+                                                loadingCategory: false
+
+                                          })
+
+                                    }
+                              );
+                        },
+                  );
+            }
+
+
+            // getData()
             getData()
+            categories()
 
       }, [])
 
 
+      console.log('categorioes', cateData.categories)
+      console.log('treaments', data.treatment_data)
+
 
       return (
             <View style={styles.container}>
-                  <ScrollView>
-                        <TouchableOpacity onPress={() => navigation.navigate('Details',
+                  {data.isLoading  || cateData.loadingCategory ? <ActivityIndicator /> :
 
-                        )}>
-                              <Text style={styles.item}>hello</Text>
-                        </TouchableOpacity>
+                        <ScrollView>
+                              {cateData.categories.map((item) => (
+                                    <View key={item.id}>
+                                          <TouchableOpacity onPress={() => navigation.navigate('Details',
+                                                {
 
-                  </ScrollView>
+                                                      treatment_data: item,
+                                                }
+                                          )}>
+                                                <Text style={styles.item}>{item.id}</Text>
+                                          </TouchableOpacity>
+                                    </View>
+
+                              ))}
+
+                              {data.treatment_data.map((item) => (
+                                    <View key={item.id}>
+                                          <TouchableOpacity onPress={() => navigation.navigate('Details',
+                                                {
+
+                                                      treatment_data: item,
+                                                }
+                                          )}>
+                                                <Text style={styles.item}>{item.indication_name}</Text>
+                                          </TouchableOpacity>
+                                    </View>
+
+                              ))}
+
+                        </ScrollView>
+                  }
             </View>
       )
 
