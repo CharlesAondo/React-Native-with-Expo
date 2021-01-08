d558249b6d0f8df389a87858201ae4a4105d0e5a
charlestimeless <54895123+charlestimeless@users.noreply.github.com>
2021-01-08 11:23:32 -0400

    Created expo shared project for testing by james

--- hooks/useDatabase.js
+++ hooks/useDatabase.js
@@ -25,7 +25,7 @@ const useDatabase = () => {
       useEffect(() => {
             async function loadDataAsync() {
                   try {
-/*
+
                         // await database.dropDatabaseTablesAsync();
                         await database.setupCreateTablesAsync();
 
@@ -48,7 +48,7 @@ const useDatabase = () => {
                         await formulations.insertFormulationsAsync();
                         await soundalikes.insertSoundAlikesAsync();
                         await formulationSpecie.insertFormulationSpecieAsync();
-*/
+
 
                         setDBLoadingComplete(true);
                   } catch (e) {
